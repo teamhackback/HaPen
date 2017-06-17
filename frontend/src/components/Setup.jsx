474bc98955db374ea40e285b9f0cabade90b93e9
phejny <ph@towns,cz>
2017-06-17 16:57:40 +0200

    [Frontend] Select project type

--- frontend/src/components/Setup.jsx
+++ frontend/src/components/Setup.jsx
@@ -7,6 +7,9 @@ import {
     StepLabel,
 } from 'material-ui/Stepper';
 import Dialog from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 
 export default class Setup extends Component {
@@ -48,6 +51,26 @@ export default class Setup extends Component {
                                 <StepLabel>Select project language</StepLabel>
                             </Step>
                         </Stepper>
+
+
+
+
+                        <TextField
+                            floatingLabelText="Project name"
+                        />
+
+
+                        <SelectField
+                            floatingLabelText="Frequency"
+                            value={0}
+                            onChange={()=>{}}
+                        >
+
+                            {['Web app', 'Mobile app'].map((area,index)=>(
+                                <MenuItem key={index} value={index} primaryText={area} />
+                            ))}
+                        </SelectField>
+
 
 
                     </Dialog>
