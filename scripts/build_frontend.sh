489fef86371f93688529f211b9be103f4dbf9eec
Jait Dixit <jait.dixit@tum.de>
2017-06-17 19:42:26 +0200

    Enable and fix frontend production build

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$NODE_ENV" == "production" ]
+if [ "$NODE_ENV" == production ]
 then
         cd frontend && npm install --global yarn > /dev/null 2>&1 && yarn install && yarn build
 fi
