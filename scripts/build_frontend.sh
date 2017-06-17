2877914949a8fc79fe62f175bdb330ac4dc0432b
Jait Dixit <jait.dixit@tum.de>
2017-06-17 17:10:40 +0200

    Quiet npm install and build frontend only in production

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-cd frontend && npm install --global yarn && yarn install && yarn build
+if [ "$NODE_ENV" == "production" ]
+then
+        cd frontend && npm install --global yarn > /dev/null 2>&1 && yarn install && yarn build
+fi
