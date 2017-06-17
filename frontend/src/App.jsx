1b234cf55cf877833b87adec8a154209855b467d
phejny <ph@towns,cz>
2017-06-17 15:42:34 +0200

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+
+import './App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+
+                <nav className="top">
+                    <img src={logo} className="logo" alt="HaPen logo"/>
+                    <h1 className="App-title"> HaPen </h1>
+                    <div className="App-subtitle"> Base for opensource</div>
+                </nav>
+
+                <main>
+                    <div className="window">
+
+                        <h2 className="title">Start a new project</h2>
+
+                        <div className="content">
+
+                            Project name:
+                            <input type="text"/>
+
+                            Select your language:
+                            <select>
+                                {['JavaScript', 'TypeScript'].map((language)=>(
+                                    <option key={language}>{language}</option>
+                                ))}
+                            </select>
+
+                            Select project area:
+                            <select>cd frontend
+
+                                {['Web app', 'Mobile app'].map((area)=>(
+                                    <option key={area}>{area}</option>
+                                ))}
+                            </select>
+                        </div>
+
+
+                    </div>
+                </main>
+
+
+                <footer>
+                    Made on HackPrague
+                </footer>
+
+            </div>
+        )
+    }
+}
+
+export default App;
