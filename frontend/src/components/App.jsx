db2cc9bcefde19cdde5e20f5fac7f046ad559289
phejny <ph@towns,cz>
2017-06-17 16:09:29 +0200

    [Frontend] material ui

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,55 +1,66 @@
 import React, {Component} from 'react';
 import logo from '../assets/logo.svg';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 import './App.scss';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
+            <MuiThemeProvider>
+                <div className="App">
 
-                <nav className="top">
-                    <img src={logo} className="logo" alt="HaPen logo"/>
-                    <h1 className="App-title"> HaPen </h1>
-                    <div className="App-subtitle"> Base for opensource</div>
-                </nav>
+                    <nav className="top">
+                        <img src={logo} className="logo" alt="HaPen logo"/>
+                        <h1 className="App-title"> HaPen </h1>
+                        <div className="App-subtitle"> Base for opensource</div>
+                    </nav>
 
-                <main>
-                    <div className="window">
+                    <main>
+                        <div className="window">
 
-                        <h2 className="title">Start a new project</h2>
+                            <h2 className="title">Start a new project</h2>
 
-                        <div className="content">
+                            <div className="content">
 
-                            Project name:
-                            <input type="text"/>
+                                Project name:
+                                <input type="text"/>
 
-                            Select your language:
-                            <select>
-                                {['JavaScript', 'TypeScript'].map((language)=>(
-                                    <option key={language}>{language}</option>
-                                ))}
-                            </select>
+                                Select your language:
+                                <select>
+                                    {['JavaScript', 'TypeScript'].map((language)=>(
+                                        <option key={language}>{language}</option>
+                                    ))}
+                                </select>
 
-                            Select project area:
-                            <select>cd frontend
+                                Select project area:
+                                <select>cd frontend
 
-                                {['Web app', 'Mobile app'].map((area)=>(
-                                    <option key={area}>{area}</option>
-                                ))}
-                            </select>
+                                    {['Web app', 'Mobile app'].map((area)=>(
+                                        <option key={area}>{area}</option>
+                                    ))}
+                                </select>
+
+
+
+                                <RaisedButton label="Submit" />
+
+
+                            </div>
+
+
                         </div>
+                    </main>
 
 
-                    </div>
-                </main>
+                    <footer>
+                        Made on HackPrague
+                    </footer>
 
-
-                <footer>
-                    Made on HackPrague
-                </footer>
-
-            </div>
+                </div>
+            </MuiThemeProvider>
         )
     }
 }
